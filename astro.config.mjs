8d972757c410bcc2992b24df2386918fa6e3f454
kama2vern <s.wyvern.acespring@gmail.com>
2025-03-20 15:50:11 +0900

    GitHub Pages用の設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kama2vern.github.io',
+  output: 'static',
+});
